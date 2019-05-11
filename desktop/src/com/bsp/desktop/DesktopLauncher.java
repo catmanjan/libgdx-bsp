0bbbb5c790d2c1ac16b6e63041ebcd00c6fabf61
Jan Martin <Jan.Martin@icognition.com.au>
2019-05-11 22:34:21 +1000

    initial

---
+++ desktop/src/com/bsp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bsp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bsp.BspGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BspGame(), config);
+	}
+}
