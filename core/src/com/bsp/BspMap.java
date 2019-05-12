650a19b0e791805158e74280cb79e00a3f5b92e4
Jan Martin <jan.martin@icognition.com.au>
2019-05-12 23:27:18 +1000

    trying to fix texture coordinates

--- core/src/com/bsp/BspMap.java
+++ core/src/com/bsp/BspMap.java
@@ -1,6 +1,14 @@
 package com.bsp;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -9,24 +17,37 @@ import java.nio.ByteOrder;
 
 public class BspMap {
 
-    public BspHeader header;
-    public BspVertex[] vertices;
-    public BspFace[] faces;
-    public BspEdge[] edges;
+    // bsp components
+    private BspHeader header;
+    private BspVertex[] vertices;
+    private BspFace[] faces;
+    private int[] faceEdges;
+    private BspEdge[] edges;
+    private BspPlane[] planes;
+    private BspTextureInformation[] textures;
 
+    // file reading bits
     private int bufferSize = 1024;
     private BufferedInputStream stream;
     private byte[] buffer = new byte[bufferSize];
 
+    // libgdx mesh
+    private Mesh mesh;
+    private Texture texture;
+
     public BspMap(FileHandle file) {
         try {
             load(file);
+            generateMesh();
+            texture = new Texture(Gdx.files.internal("assets/grnx2_9.jpg"), true);
+            texture.setFilter(Texture.TextureFilter.MipMap, Texture.TextureFilter.Nearest);
+            texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void load(FileHandle file) throws IOException {
+    private void load(FileHandle file) throws IOException {
         byte[] bytes = file.readBytes();
 
         stream = file.read(bufferSize);
@@ -50,9 +71,9 @@ public class BspMap {
 
         for (int i = 0; i < vertices.length; i++) {
             vertices[i] = new BspVertex();
-            vertices[i].x = readNextFloat();
-            vertices[i].y = readNextFloat();
             vertices[i].z = readNextFloat();
+            vertices[i].x = readNextFloat();
+            vertices[i].y = -readNextFloat();
         }
 
         // faces
@@ -86,6 +107,173 @@ public class BspMap {
             edges[i].index1 = readNextShort();
             edges[i].index2 = readNextShort();
         }
+
+        // faces edges
+        stream = file.read(bufferSize);
+        stream.skip(header.lump[12].offset);
+
+        faceEdges = new int[header.lump[12].length / 4];
+
+        for (int i = 0; i < faceEdges.length; i++) {
+            faceEdges[i] = readNextInt();
+        }
+
+        // planes
+        stream = file.read(bufferSize);
+        stream.skip(header.lump[1].offset);
+
+        planes = new BspPlane[header.lump[1].length / 20];
+
+        for (int i = 0; i < planes.length; i++) {
+            planes[i] = new BspPlane();
+            planes[i].normal = new BspPoint3f();
+            planes[i].normal.x = readNextFloat();
+            planes[i].normal.y = readNextFloat();
+            planes[i].normal.z = readNextFloat();
+            planes[i].distance = readNextFloat();
+            planes[i].type = readNextInt();
+        }
+
+        // textures
+        stream = file.read(bufferSize);
+        stream.skip(header.lump[5].offset);
+
+        textures = new BspTextureInformation[header.lump[5].length / 76];
+
+        for (int i = 0; i < textures.length; i++) {
+            textures[i] = new BspTextureInformation();
+            textures[i].uAxis = new BspPoint3f();
+            textures[i].uAxis.x = readNextFloat();
+            textures[i].uAxis.y = readNextFloat();
+            textures[i].uAxis.z = readNextFloat();
+            textures[i].uOffset = readNextFloat();
+            textures[i].vAxis = new BspPoint3f();
+            textures[i].vAxis.x = readNextFloat();
+            textures[i].vAxis.y = readNextFloat();
+            textures[i].vAxis.z = readNextFloat();
+            textures[i].vOffset = readNextFloat();
+            textures[i].flags = readNextInt();
+            textures[i].value = readNextInt();
+            textures[i].textureName = readNextString(32);
+            textures[i].nextTextureInformation = readNextInt();
+        }
+    }
+
+    private void generateMesh() {
+        int numberOfIndices = 0;
+
+        for (int i = 0; i < faces.length; i++) {
+            numberOfIndices += faces[i].numEdges * 2;
+        }
+
+        float[] meshVertices = new float[vertices.length * 6];
+        short[] meshIndices = new short[numberOfIndices];
+
+        for (int i = 0; i < vertices.length; i++) {
+            meshVertices[i * 6 + 0] = vertices[i].x;
+            meshVertices[i * 6 + 1] = vertices[i].y;
+            meshVertices[i * 6 + 2] = vertices[i].z;
+            meshVertices[i * 6 + 3] = Color.argb8888(Color.WHITE);
+            meshVertices[i * 6 + 4] = 0;
+            meshVertices[i * 6 + 5] = 0;
+        }
+
+        int idx = 0;
+
+        float texs = 64;
+
+        for (int i = 0; i < faces.length; i++) {
+            faces[i].indicesOffset = idx;
+
+            BspTextureInformation texture = textures[faces[i].textureInfo];
+
+            for (int j = faces[i].firstEdge; j < faces[i].firstEdge + faces[i].numEdges; j++) {
+                if (faceEdges[j] > 0) {
+                    meshIndices[idx++] = edges[faceEdges[j]].index1;
+                    meshIndices[idx++] = edges[faceEdges[j]].index2;
+
+                    // gross dotproducts
+                    meshVertices[edges[faceEdges[j]].index1 * 6 + 4] =
+                            meshVertices[edges[faceEdges[j]].index1 * 6 + 0] * texture.uAxis.x +
+                                    meshVertices[edges[faceEdges[j]].index1 * 6 + 1] * texture.uAxis.y +
+                                    meshVertices[edges[faceEdges[j]].index1 * 6 + 2] * texture.uAxis.z +
+                                    texture.uOffset;
+                    meshVertices[edges[faceEdges[j]].index1 * 6 + 4] /= texs;
+
+                    meshVertices[edges[faceEdges[j]].index1 * 6 + 5] =
+                            meshVertices[edges[faceEdges[j]].index1 * 6 + 0] * texture.vAxis.x +
+                                    meshVertices[edges[faceEdges[j]].index1 * 6 + 1] * texture.vAxis.y +
+                                    meshVertices[edges[faceEdges[j]].index1 * 6 + 2] * texture.vAxis.z +
+                                    texture.vOffset;
+                    meshVertices[edges[faceEdges[j]].index1 * 6 + 5] /= texs;
+
+                    meshVertices[edges[faceEdges[j]].index2 * 6 + 4] =
+                            meshVertices[edges[faceEdges[j]].index2 * 6 + 0] * texture.uAxis.x +
+                                    meshVertices[edges[faceEdges[j]].index2 * 6 + 1] * texture.uAxis.y +
+                                    meshVertices[edges[faceEdges[j]].index2 * 6 + 2] * texture.uAxis.z +
+                                    texture.uOffset;
+                    meshVertices[edges[faceEdges[j]].index2 * 6 + 4] /= texs;
+
+                    meshVertices[edges[faceEdges[j]].index2 * 6 + 5] =
+                            meshVertices[edges[faceEdges[j]].index2 * 6 + 0] * texture.vAxis.x +
+                                    meshVertices[edges[faceEdges[j]].index2 * 6 + 1] * texture.vAxis.y +
+                                    meshVertices[edges[faceEdges[j]].index2 * 6 + 2] * texture.vAxis.z +
+                                    texture.vOffset;
+                    meshVertices[edges[faceEdges[j]].index2 * 6 + 5] /= texs;
+                } else {
+                    meshIndices[idx++] = edges[-faceEdges[j]].index2;
+                    meshIndices[idx++] = edges[-faceEdges[j]].index1;
+
+                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 4] =
+                            meshVertices[edges[-faceEdges[j]].index2 * 6 + 0] * texture.uAxis.x +
+                                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 1] * texture.uAxis.y +
+                                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 2] * texture.uAxis.z +
+                                    texture.uOffset;
+                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 4] /= texs;
+
+                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 5] =
+                            meshVertices[edges[-faceEdges[j]].index2 * 6 + 0] * texture.vAxis.x +
+                                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 1] * texture.vAxis.y +
+                                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 2] * texture.vAxis.z +
+                                    texture.vOffset;
+                    meshVertices[edges[-faceEdges[j]].index2 * 6 + 5] /= texs;
+
+                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 4] =
+                            meshVertices[edges[-faceEdges[j]].index1 * 6 + 0] * texture.uAxis.x +
+                                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 1] * texture.uAxis.y +
+                                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 2] * texture.uAxis.z +
+                                    texture.uOffset;
+                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 4] /= texs;
+
+                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 5] =
+                            meshVertices[edges[-faceEdges[j]].index1 * 6 + 0] * texture.vAxis.x +
+                                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 1] * texture.vAxis.y +
+                                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 2] * texture.vAxis.z +
+                                    texture.vOffset;
+                    meshVertices[edges[-faceEdges[j]].index1 * 6 + 5] /= texs;
+                }
+
+                faces[i].indicesCount += 2;
+            }
+        }
+
+        mesh = new Mesh(true, meshVertices.length, meshIndices.length,
+                new VertexAttribute(VertexAttributes.Usage.Position, 3, "a_position"),
+                new VertexAttribute(VertexAttributes.Usage.ColorPacked, 4, "a_color"),
+                new VertexAttribute(VertexAttributes.Usage.TextureCoordinates, 2, "a_texCoords"));
+        mesh.setVertices(meshVertices);
+        mesh.setIndices(meshIndices);
+    }
+
+    public void render(ShaderProgram shader) {
+        texture.bind();
+        shader.setUniformi("u_texture", 0);
+
+        // gross, have to do this because libgdx doesn't support glDrawArray in mesh render
+        // nor primitive restart indices (yet?)
+        for (int i = 0; i < faces.length; i++) {
+            mesh.render(shader, GL20.GL_TRIANGLE_FAN, faces[i].indicesOffset, faces[i].indicesCount);
+        }
     }
 
     private String readNextString(int length) throws IOException {
@@ -118,36 +306,62 @@ public class BspMap {
         return buffer[0];
     }
 
+    class BspTextureInformation {
+        BspPoint3f uAxis;
+        float uOffset;
+        BspPoint3f vAxis;
+        float vOffset;
+        int flags;
+        int value;
+        String textureName;
+        int nextTextureInformation;
+    }
+
+    class BspPoint3f {
+        float x;
+        float y;
+        float z;
+    }
+
+    class BspPlane {
+        BspPoint3f normal;
+        float distance;
+        int type;
+    }
+
     class BspEdge {
-        public short index1;
-        public short index2;
+        short index1;
+        short index2;
     }
 
     class BspFace {
-        public short plane;
-        public short planeSide;
-        public int firstEdge;
-        public short numEdges;
-        public short textureInfo;
-        public byte[] lightmapStyles = new byte[4];
-        public int lightmapOffset;
+        short plane;
+        short planeSide;
+        int firstEdge;
+        short numEdges;
+        short textureInfo;
+        byte[] lightmapStyles = new byte[4];
+        int lightmapOffset;
+
+        // special properties for libgdx mesh rendering
+        int indicesOffset;
+        int indicesCount;
     }
 
     class BspVertex {
-        public float x;
-        public float y;
-        public float z;
+        float x;
+        float y;
+        float z;
     }
 
     class BspHeader {
-        public String magic;
-        public int version;
-        public BspLump[] lump = new BspLump[19];
+        String magic;
+        int version;
+        BspLump[] lump = new BspLump[19];
     }
 
     class BspLump {
-        public int offset;
-        public int length;
+        int offset;
+        int length;
     }
-
 }
--- core/src/com/bsp/GameScreen.java
+++ core/src/com/bsp/GameScreen.java
@@ -5,42 +5,49 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector3;
 
 public class GameScreen extends ScreenAdapter implements InputProcessor {
 
-    Mesh mesh;
-    ShaderProgram shader;
-    PerspectiveCamera camera;
     String vertexShader = "attribute vec4 a_position;    \n" +
+            "attribute vec4 a_color;\n" +
+            "attribute vec2 a_texCoords;\n" +
             "uniform mat4 u_projTrans;\n" +
+            "varying vec4 v_color;" +
+            "varying vec2 v_texCoords;" +
             "void main()                  \n" +
             "{                            \n" +
+            "   v_color = a_color; \n" +
+            "   v_texCoords = a_texCoords; \n" +
             "   gl_Position =  u_projTrans * a_position;  \n" +
             "}                            \n";
     String fragmentShader = "#ifdef GL_ES\n" +
             "precision mediump float;\n" +
             "#endif\n" +
+            "varying vec4 v_color;\n" +
+            "varying vec2 v_texCoords;\n" +
+            "uniform sampler2D u_texture;\n" +
             "void main()                                  \n" +
             "{                                            \n" +
-            "  gl_FragColor = vec4(1,1,1,1);\n" +
+            "  gl_FragColor = texture2D(u_texture, v_texCoords);\n" +
             "}";
+    private BspMap bsp;
+
     private int mouseX;
     private int mouseY;
     private boolean moving;
     private float rotationSpeed = 0.3f;
-    private float movementSpeed = 0.3f;
+    private ShaderProgram shader;
+    private PerspectiveCamera camera;
+    private float movementSpeed = 2.0f;
 
     @Override
     public void show() {
         camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.near = 0.005f;
-        camera.far = 300;
+        camera.near = 1.0f;
+        camera.far = 9000.0f;
 
         Gdx.input.setInputProcessor(this);
         Gdx.input.setCursorCatched(true);
@@ -48,78 +55,7 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
         ShaderProgram.pedantic = false;
         shader = new ShaderProgram(vertexShader, fragmentShader);
 
-        BspMap map = new BspMap(Gdx.files.internal("assets/q2dm1.bsp"));
-
-        float[] vertices = new float[map.vertices.length * 3];
-        short[] indices = new short[map.edges.length * 2];
-
-        for (int i = 0; i < map.vertices.length; i++) {
-            // scaling down by 1/10th here so it fits in the camera viewport
-            vertices[i * 3 + 0] = map.vertices[i].x / 10;
-            vertices[i * 3 + 1] = map.vertices[i].y / 10;
-            vertices[i * 3 + 2] = map.vertices[i].z / 10;
-        }
-
-        for (int i = 0; i < map.edges.length; i++) {
-            indices[i * 2 + 0] = map.edges[i].index1;
-            indices[i * 2 + 1] = map.edges[i].index2;
-        }
-
-        mesh = new Mesh(true, vertices.length, indices.length,
-                new VertexAttribute(VertexAttributes.Usage.Position, 3, "a_position"));
-        mesh.setVertices(vertices);
-        mesh.setIndices(indices);
-    }
-
-    @Override
-    public void render(float delta) {
-        if (moving) {
-            camera.position.add(camera.direction.x * movementSpeed,
-                    camera.direction.y * movementSpeed,
-                    camera.direction.z * movementSpeed);
-            camera.update();
-        }
-
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-        shader.begin();
-        shader.setUniformMatrix("u_projTrans", camera.combined);
-        mesh.render(shader, GL20.GL_LINES);
-        shader.end();
-    }
-
-    private boolean processMouseLook(int screenX, int screenY) {
-        int magX = Math.abs(mouseX - screenX);
-        int magY = Math.abs(mouseY - screenY);
-
-        if (mouseX > screenX) {
-            camera.rotate(Vector3.Y, 1 * magX * rotationSpeed);
-            camera.update();
-        }
-
-        if (mouseX < screenX) {
-            camera.rotate(Vector3.Y, -1 * magX * rotationSpeed);
-            camera.update();
-        }
-
-        if (mouseY < screenY) {
-            if (camera.direction.y > -0.965)
-                camera.rotate(camera.direction.cpy().crs(Vector3.Y), -1 * magY * rotationSpeed);
-            camera.update();
-        }
-
-        if (mouseY > screenY) {
-            if (camera.direction.y < 0.965)
-                camera.rotate(camera.direction.cpy().crs(Vector3.Y), 1 * magY * rotationSpeed);
-            camera.update();
-        }
-
-        mouseX = screenX;
-        mouseY = screenY;
-
-        return false;
+        bsp = new BspMap(Gdx.files.internal("assets/base1.bsp"));
     }
 
     @Override
@@ -167,6 +103,59 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
 
     @Override
     public boolean scrolled(int amount) {
+        return false;
+    }
+
+    @Override
+    public void render(float delta) {
+        if (moving) {
+            camera.position.add(camera.direction.x * movementSpeed,
+                    camera.direction.y * movementSpeed,
+                    camera.direction.z * movementSpeed);
+            camera.update();
+        }
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+        Gdx.gl.glEnable(GL20.GL_CULL_FACE);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glCullFace(GL20.GL_BACK);
+
+        shader.begin();
+        shader.setUniformMatrix("u_projTrans", camera.combined);
+        bsp.render(shader);
+        shader.end();
+    }
+
+    private boolean processMouseLook(int screenX, int screenY) {
+        int magX = Math.abs(mouseX - screenX);
+        int magY = Math.abs(mouseY - screenY);
+
+        if (mouseX > screenX) {
+            camera.rotate(Vector3.Y, -1 * magX * rotationSpeed);
+            camera.update();
+        }
+
+        if (mouseX < screenX) {
+            camera.rotate(Vector3.Y, 1 * magX * rotationSpeed);
+            camera.update();
+        }
+
+        if (mouseY < screenY) {
+            if (camera.direction.y < 0.965)
+                camera.rotate(camera.direction.cpy().crs(Vector3.Y), 1 * magY * rotationSpeed);
+            camera.update();
+        }
+
+        if (mouseY > screenY) {
+            if (camera.direction.y > -0.965)
+                camera.rotate(camera.direction.cpy().crs(Vector3.Y), -1 * magY * rotationSpeed);
+            camera.update();
+        }
+
+        mouseX = screenX;
+        mouseY = screenY;
+
         return false;
     }
 }
